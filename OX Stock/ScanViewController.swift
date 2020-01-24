e6ba84fa9c394bf2927dd214b5c9d0c5e80ffce9
tlewinzon-sol <tlewinzon@solstice.com>
2020-01-24 15:59:05 -0300

    Initial commit. App template.

---
+++ OX Stock/ScanViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScanViewController.swift
+//  OX Stock
+//
+//  Created by Tobias Lewinzon on 24/01/2020.
+//  Copyright © 2020 tobiaslewinzon. All rights reserved.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OX Stock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OX Stock
+//
+//  Created by Tobias Lewinzon on 24/01/2020.
+//  Copyright © 2020 tobiaslewinzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
